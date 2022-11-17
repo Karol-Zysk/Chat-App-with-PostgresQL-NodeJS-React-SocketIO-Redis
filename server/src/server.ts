a95762018872216c774d6804f93e94a202bb6a1b
Karol Zyśk <trener.karolzysk@gmail.com>
2022-11-17 09:33:29 +0100

    signup and login authentication with bcrypt and express-session

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,7 @@ import http from "http";
 import { Server } from "socket.io";
 import { port } from "./config/config";
 import authRouter from "./routes/authRouter";
+import session from "express-session";
 dotenv.config({ path: "config.env" });
 
 const app = express();
@@ -16,8 +17,22 @@ const io = new Server(server, {
 });
 
 app.use(helmet());
-app.use(express.json());
 app.use(cors({ origin: "http://127.0.0.1:5173", credentials: true }));
+app.use(express.json());
+app.use(
+  session({
+    cookie: {
+      secure: true,
+      httpOnly: true,
+      expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+      sameSite: process.env.ENVIRONMENT === "production" ? "none" : "lax",
+    },
+    secret: `${process.env.COOKIE_SECRET}`,
+    name: "sid",
+    saveUninitialized: false,
+    resave: false,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("elo elo");
